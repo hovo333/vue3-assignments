12595e24bbae3c9506c813ca3dcdae273804158d
hovo333 <hovhannes.khazaryan@gmail.com>
2022-09-20 23:30:20 +0400

    Was integrated slots ability for dropdown component.

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -2,51 +2,59 @@
   <div class="relative">
     <div class="flex">
       <button class="z-10 relative flex items-center focus:outline-none select-none">
-        <div @click="open = !open">
-          <span class="px-2 py-2 border rounded inline-flex items-center text-sm">
-            <span class="mr-2">{{ dropdownTitle }}</span>
+        <div class="flex">
+          <div @click="open = !open">
+            <slot name="button" v-if="customButton"></slot>
 
-            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path d="M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 0 1-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z" />
-            </svg>
-          </span>
+            <span v-else class="px-2 py-2 border rounded inline-flex items-center text-sm">
+              <span class="mr-2">{{ dropdownTitle }}</span>
+
+              <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 0 1-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z" />
+              </svg>
+            </span>
+          </div>
+
+          <slot name="filter" v-if="customFilter"></slot>
+          <div class="flex hg-filter-content" v-else-if="!customContent">
+            <img id="reorderIcon"
+                 v-if="!order"
+                 :class="{'hg-disabled-icon': !open}"
+                 title="Order"
+                 src="../assets/images/icon-sort.svg"
+                 alt="reorderOrder"
+                 v-on:click="reorder"
+            />
+            <img id="reorderIcon"
+                 v-else
+                 :class="{'hg-disabled-icon': !open}"
+                 src="../assets/images/icon-sort-active.svg"
+                 :title="order === 1 ? 'Reverse order' : 'Cancel order'"
+                 alt="reorderOrder"
+                 v-on:click="reorder"
+            />
+
+            <img id="showAll"
+                 v-if="!isShowAll"
+                 :class="{'hg-disabled-icon': !open}"
+                 src="../assets/images/icon-show-all.svg"
+                 title="Show all items"
+                 width="25"
+                 alt="reorderOrder"
+                 v-on:click="showAll"
+            />
+            <img id="showAll"
+                 v-else
+                 :class="{'hg-disabled-icon': !open}"
+                 src="../assets/images/icon-show-all-active.svg"
+                 title="Hide hidden items"
+                 width="25"
+                 alt="reorderOrder"
+                 v-on:click="showAll"
+            />
+          </div>
         </div>
 
-        <img id="reorderIcon"
-             v-if="!order"
-             :class="{'hg-disabled-icon': !open}"
-             title="Order"
-             src="../assets/images/icon-sort.svg"
-             v-on:click="reorder"
-             alt="reorderOrder"
-        />
-        <img id="reorderIcon"
-             v-else
-             :class="{'hg-disabled-icon': !open}"
-             src="../assets/images/icon-sort-active.svg"
-             :title="order === 1 ? 'Reverse order' : 'Cancel order'"
-             v-on:click="reorder"
-             alt="reorderOrder"
-        />
-
-        <img id="showAll"
-             v-if="!isShowAll"
-             :class="{'hg-disabled-icon': !open}"
-             src="../assets/images/icon-show-all.svg"
-             title="Show all items"
-             width="25"
-             alt="reorderOrder"
-             v-on:click="showAll"
-        />
-        <img id="showAll"
-             v-else
-             :class="{'hg-disabled-icon': !open}"
-             src="../assets/images/icon-show-all-active.svg"
-             title="Hide hidden items"
-             width="25"
-             alt="reorderOrder"
-             v-on:click="showAll"
-        />
       </button>
     </div>
 
@@ -54,23 +62,28 @@
     <button class="fixed inset-0 h-full w-full cursor-default focus:outline-none" v-if="open" @click="open = false" tabindex="-1"></button>
     <!--dropdown content: desktop-->
     <transition enter-active-class="transition-all duration-200 ease-out" leave-active-class="transition-all duration-750 ease-in" enter-class="opacity-0 scale-75" enter-to-class="opacity-100 scale-100" leave-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-75">
-      <div class="hidden md:block absolute shadow-lg border w-48 rounded py-1 px-2 text-sm mt-4 bg-white" :class="placement === 'right' ? 'right-0' : 'left-0'" v-if="open && dropdownItems">
-        <a v-for="(item, index) in dropdownItems"
+      <div class="hidden md:block absolute shadow-lg border w-48 rounded py-1 px-2 text-sm mt-4 bg-white hg-drop-down-content" :class="placement === 'right' ? 'right-0' : 'left-0'" v-if="open">
+        <slot name="content" v-if="customContent"></slot>
+        <a v-else
+           v-for="(item, index) in dropdownItems"
            v-bind:key="`dropdown_${index}`"
            class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-indigo-600 hover:text-white"
            href="#"
-           @click="$emit('select', item.id)"
+           @click="selectItem(item.id)"
         >{{ item.id }}</a>
       </div>
     </transition>
 
     <!--dropdown content: mobile-->
     <transition enter-active-class="transition-all duration-200 ease-out" leave-active-class="transition-all duration-750 ease-in" enter-class="opacity-0 scale-75" enter-to-class="opacity-100 scale-100" leave-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-75">
-      <div class="md:hidden fixed inset-x-0 bottom-0 bg-white w-full z-20 px-2 py-2 shadow-2xl leading-loose" v-if="open && dropdownItems">
-        <a v-for="(item, index) in dropdownData"
+      <div class="md:hidden fixed inset-x-0 bottom-0 bg-white w-full z-20 px-2 py-2 shadow-2xl leading-loose hg-drop-down-content" v-if="open">
+        <slot name="content" v-if="customContent"></slot>
+        <a v-else
+           v-for="(item, index) in dropdownData"
            v-bind:key="`dropdown_${index}`"
            class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-indigo-600 hover:text-white"
            href="#"
+           @click="selectItem(item.id)"
         >{{ item.id }}</a>
       </div>
     </transition>
@@ -86,6 +99,10 @@ const emitter = mitt()
 export default {
   name: "Dropdown-component",
   props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
     placement: {
       type: String,
       default: "left",
@@ -97,7 +114,19 @@ export default {
     },
     dropdownData: {
       type: Array,
-      required: true
+      required: false
+    },
+    customButton: {
+      type: Boolean,
+      default: false
+    },
+    customContent: {
+      type: Boolean,
+      default: false
+    },
+    customFilter: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
@@ -111,10 +140,17 @@ export default {
   },
   data() {
     return {
-      open: false,
+      open: this.visible,
       order: 0,
       isShowAll: false,
     };
+  },
+  watch: {
+    'visible': {
+      handler() {
+        this.open = this.visible
+      }
+    }
   },
   mounted() {
     this.init()
@@ -146,6 +182,10 @@ export default {
       emitter.on("hook:beforeDestroy", () => {
         document.removeEventListener("keydown", onEscape);
       });
+    },
+    selectItem(itemID) {
+      this.open = false
+      this.$emit('select', itemID)
     }
   }
 };
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,60 @@
 <template>
   <div class="hg-main-page">
-    <organism-dropdown v-if="dropdownData" :dropdown-data="dropdownData" @select="selectItem"/>
+    <h1>Props example</h1>
+    <div class="flex hg-dropdown-content">
+      <organism-dropdown v-if="dropdownData" :dropdown-data="dropdownData" @select="selectItem" :visible="showDropdown1"/>
+    </div>
+
+    <h1>Slots example</h1>
+    <div class="flex hg-dropdown-content">
+      <organism-dropdown v-if="dropdownData"
+                         :visible="showDropdown2"
+                         :custom-button="true"
+                         :custom-filter="true"
+                         :custom-content="true"
+      >
+        <!-- Button content -->
+        <template v-slot:button>
+          <span class="px-2 py-2 border rounded inline-flex items-center text-sm" @click="showDropdown2 = true">
+            <span class="mr-2">Dropdown2</span>
+
+            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 0 1-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z" />
+            </svg>
+          </span>
+        </template>
+
+        <!-- Button content -->
+        <template v-slot:filter>
+          <div class="flex hg-filter-content">
+            <img id="reorderIcon"
+                 v-if="!slotDropDownSettings.order"
+                 title="Order"
+                 src="../assets/images/icon-sort.svg"
+                 alt="reorderOrder"
+                 v-on:click="reorder"
+            />
+            <img id="reorderIcon"
+                 v-else
+                 src="../assets/images/icon-sort-active.svg"
+                 :title="slotDropDownSettings.order === 1 ? 'Reverse order' : 'Cancel order'"
+                 alt="reorderOrder"
+                 v-on:click="reorder"
+            />
+          </div>
+        </template>
+
+        <!-- Opened dropdown content -->
+        <template v-slot:content>
+          <a v-for="(item, index) in dropdownItems"
+             v-bind:key="`dropdown_${index}`"
+             class="flex w-full justify-between items-center rounded px-2 py-1 my-1 hover:bg-indigo-600 hover:text-white"
+             href="#"
+             @click="selectItem(item.id)"
+          >{{ item.id }}</a>
+        </template>
+      </organism-dropdown>
+    </div>
   </div>
 </template>
 
@@ -12,8 +66,19 @@ export default {
   components: {
     OrganismDropdown
   },
+  computed: {
+    dropdownItems() {
+      return this.dropdownData
+          .filter(item => this.isShowAll ? 1 === 1: item.hide === false)
+          .sort((a, b) => this.slotDropDownSettings.order === 1
+              ? a.order - b.order : this.slotDropDownSettings.order === 2
+                  ? b.order - a.order : 1 === 1)
+    }
+  },
   data() {
     return {
+      showDropdown1: false,
+      showDropdown2: false,
       dropdownTitle: 'Main',
       dropdownData: [
         {
@@ -46,12 +111,23 @@ export default {
           order: 5,
           hide: true
         }
-      ]
+      ],
+      slotDropDownSettings: {
+        order: 0
+      }
     }
   },
   methods: {
     selectItem(itemID) {
       console.log('itemID: ', itemID)
+      this.showDropdown = false
+    },
+    reorder() {
+      if (this.slotDropDownSettings.order === 2) {
+        this.slotDropDownSettings.order = 0
+      } else {
+        this.slotDropDownSettings.order++
+      }
     }
   }
 }
